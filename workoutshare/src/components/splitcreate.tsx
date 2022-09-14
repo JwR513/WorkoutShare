8cfaabd3a0d239db0cf4721f0586cf5e4d0cf8b6
Johnathan Weil <weilj872@gmail.com>
2022-09-14 10:09:58 -0400

    broken pipe error

--- workoutshare/src/components/splitcreate.tsx
+++ workoutshare/src/components/splitcreate.tsx
@@ -14,7 +14,7 @@ const[newSplit , setNewSplit ] = useState({})
 
 const SubmitHandler = async()=>{
   setNewSplit({...newSplit, owner: username})
-  CreateSplit()
+  CreateSplit(newSplit)
 }
 
 const newSplitForm = (e: any) =>{
@@ -30,11 +30,18 @@ const newSplitForm = (e: any) =>{
     <div>
     <form onSubmit={SubmitHandler}>
       <input type="text" name="name" onChange={newSplitForm} placeholder="Split Name"/>
-      <select name="muscle" defaultValue='0' onChange={newSplitForm}>
+      <select name="muscle" defaultValue='' onChange={newSplitForm}>
         {muscleInfo.map((muscle:any)=>(
           <option  key={muscle.id} value={muscle.id}>{muscle.name}</option>
         ))}
       </select>
+      <select name="user" defaultValue='' onChange={newSplitForm}>
+        {splitUserInfo.map((user:any)=>(
+          <option  key={user.id} value={user.id}>{user.username}</option>
+        ))}
+      </select>
+      <button type='submit'>Submit</button>
+
     </form>
     </div>
   )
